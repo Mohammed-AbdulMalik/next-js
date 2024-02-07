65db6ce3012f115d4026ee47246c59921642c8d0
Mohammed-AbdulMalik <158472039+Mohammed-AbdulMalik@users.noreply.github.com>
2024-02-07 13:32:34 +0530

    tut8

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,9 +1,18 @@
+"use client"
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+
+Link
 const Login=()=>{
+    const router = useRouter();
     return (
         <>
             <div>
                 Login
             </div>
+            <Link href="/">go to home page</Link>
+            <button onClick={()=>{router.push("/")}}>go to home page</button>
         </>
     )
 }
