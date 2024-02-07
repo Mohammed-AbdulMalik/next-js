00b8c9e520f2b45a70386145f3e623beb95b8396
Mohammed-AbdulMalik <158472039+Mohammed-AbdulMalik@users.noreply.github.com>
2024-02-07 14:30:26 +0530

    tut10

--- src/app/login/loginteacher/page.js
+++ src/app/login/loginteacher/page.js
@@ -7,7 +7,7 @@ const LoginTeacher=()=>{
     const router = useRouter();
     return(
         <>
-            <h1>This is Login Teacher page page</h1>
+            <h1 className="heading">This is Login Teacher page page</h1>
             <Link href="/">go to home page</Link>
             <button onClick={()=>{router.push("/")}}>Go to home Page</button>
         </>
